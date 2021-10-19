f881f8e5dca370b7bd1364e0da21f7fa5fd9e763
Beta Lin <sb2828sb@gmail.com>
2021-10-19 23:39:47 +0800

    finished project

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-test
+# 文字版的MySQL資料庫管理介面
+
+一個簡單的資料庫管理介面，透過Python來控制MySQL資料庫的新增、修改、刪除。
+
+使用pymysql來連結資料庫，用prettytable將資料表輸出成易閱讀的表格，由於更新和刪除的指令也會需要將會員資料表印出，故寫成function來重複使用。
+```
+def show_all_data():
+    clear()
+    # 搜尋資料庫找出會員列表
+    cur.execute("SELECT * FROM `member`")
+    data = cur.fetchall()
+
+    # 先準備會員列表表格
+    show_all = pt.PrettyTable(["編號", "姓名", "生日", "地址"])
+
+    # 製作表格
+    for item in data:
+        show_all.add_row([item[0], item[1], item[2], item[3]])
+    # 印出所有資料
+    print(show_all)
+    return data
+```
+
+
+## 功能選單
+
+(0) 離開程式
+(1) 顯示會員列表
+(2) 新增會員資料
+(3) 更新會員資料
+(4) 刪除會員資料
+
+## 印出的會員資料表範例
+
+![screenshot image](/screenshot.PNG)
+
+
+## 確認資訊
+
+我多加一點功能，讓新增資料後顯示新建的最新一筆資訊：
+```
+newest_id = cur.lastrowid
+cur.execute("SELECT * FROM `member` WHERE `id` = %s", newest_id)
+print(cur.fetchone(), "新增成功")
+```
+![new inserted screenshot](/new_inserted.png)
+
+
+修改和刪除後也會印出資料編號進行確認：
+![modified](/modified.PNG)
+
--- mian.py
+++ mian.py
@@ -2,16 +2,13 @@ import pymysql
 import prettytable as pt
 import os
 
-# 登入
+# 登入資料庫
 
-# id = input("請輸入資料庫帳號： ")
-# pw = input("請輸入資料庫密碼： ")
-
-id = "AI09_02"
-pw = "123"
+id = input("請輸入資料庫帳號： ")
+pw = input("請輸入資料庫密碼： ")
 
 link = pymysql.connect(
-    host="teaching-db.bo-yuan.net",  # 主機名稱
+    host="teaching-db.bo-yuan.net",
     user=id,
     passwd=pw,
     db="AI09_02",
@@ -27,7 +24,7 @@ cur = link.cursor()
 
 def clear(): return os.system('cls')
 
-# 列出所有資料
+# 列出所有資料，由於更新和刪除的指令也會需要將會員資料表印出，故寫成function來重複使用
 
 
 def show_all_data():
@@ -74,28 +71,35 @@ while True:
         data = show_all_data()
         update_id = input("請選擇你要修改的資料編號： ")
 
+        # check all_ids: 有在all_ids列表中的才執行修改、收input值，其他輸入則無動作
         all_ids = [str(item[0]) for item in data]
         if update_id in all_ids:
             name = input("請輸入會員姓名： ")
             birthday = input("請輸入會員生日： ")
             address = input("請輸入會員地址： ")
-            cur.execute("UPDATE `member` SET `name`='%s',`birthday`='%s',`address`='%s' WHERE `id`=='%s'", [
+            cur.execute("UPDATE `member` SET `name`=%s,`birthday`=%s,`address`=%s WHERE `id`=%s", [
                         name, birthday, address, int(update_id)])
             link.commit()
-        clear()
-        print(f"編號{delete_id}資料已修改")
+            clear()
+            print(f"編號{update_id}資料已修改")
+        else:
+            clear()
 
     elif command == "4":
         data = show_all_data()
         delete_id = input("請選擇你要刪除的資料編號： ")
 
-        # check all_ids: 有在all_ids列表中的材質行刪除，其他輸入值無動作
+        # check all_ids: 有在all_ids列表中的才執行刪除，其他輸入值無動作
         all_ids = [str(item[0]) for item in data]
         if delete_id in all_ids:
-            cur.execute("DELETE FROM `member` WHERE `id`='%s'", int(delete_id))
+            cur.execute("DELETE FROM `member` WHERE `id`=%s", int(delete_id))
             link.commit()
-        clear()
-        print(f"編號{delete_id}資料已刪除")
+            clear()
+            print(f"編號{delete_id}資料已刪除")
+        else:
+            clear()
 
     elif command == "0":
         break
+
+# Here's my GitHub repository link: https://github.com/linbeta/simple-mysql-text-interface.git
